dba22101ef4c7baa4d28bdb6687f3612906a09e7
stizianel <stizianel@gmail.com>
2021-03-17 12:36:49 +0100

    ripreso dopo perdita pc

---
+++ Alperia-ISU-Lib/Zs4hAccise.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alperia_ISU_Lib
+{
+    public class Zs4hAccise
+    {
+        public string MANDT { get; set; }
+        public string SPARTE { get; set; }
+        public string POD { get; set; }
+        public string BP { get; set; }
+        public string ANLAGE { get; set; }
+        public string COD_ESENZIONE { get; set; }
+        public string BIS { get; set; }
+        public string AB { get; set; }
+        public string VERTRAG { get; set; }
+        public string CITY_CODE { get; set; }
+        public string NAME_ORG1 { get; set; }
+        public string GRID_ID { get; set; }
+        public string COD_ACC_OBBL { get; set; }
+        public string PIVA { get; set; }
+        public string PERC_ESENZ { get; set; }
+        public string QUAN_ESENZ { get; set; }
+        public string ASS_ACC { get; set; }
+        public string ESE_ACC { get; set; }
+        public string ASS_AAC { get; set; }
+        public string ESE_AAC { get; set; }
+        public string ASS_IEC { get; set; }
+        public string ESE_IEC { get; set; }
+        public string ASA_IEC { get; set; }
+        public string ERDAT { get; set; }
+        public string ERTIM { get; set; }
+        public string ERNAM { get; set; }
+
+
+    }
+    public class Zs4hAcciseContext
+    {
+        private readonly IMongoDatabase _db;
+
+        public Zs4hAcciseContext()
+        {
+            MongoClient client = new MongoClient();
+            _db = client.GetDatabase("Alperia");
+            _db.GetCollection<Zs4hAccise>("Zs4hAccise");
+        }
+
+        public IMongoCollection<Zs4hAccise> AcciseCollection => _db.GetCollection<Zs4hAccise>("Zs4hAccise");
+    }
+}
---
+++ LoadAcciseFromZitau/Program.cs
@@ -0,0 +1,42 @@
+﻿using Alperia_ISU_Lib;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace LoadAcciseFromZitau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var ctx = new Zs4hAcciseContext();
+            var reader = new StreamReader("c:\\$work\\Temp\\zitau.csv");
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csv.Configuration.Delimiter = "|";
+            csv.Configuration.IgnoreQuotes = true;
+            csv.Configuration.TrimOptions = TrimOptions.Trim;
+
+            var lRec = ProcessFile(csv);
+            Console.WriteLine("Fine programma");
+        }
+
+        private static List<Zs4hAccise> ProcessFile(CsvReader csv)
+        {
+            try
+            {
+                var zRecs = csv.GetRecords<Zs4hAccise>().ToList();
+                return zRecs;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Errore su file {0}", "Zitau");
+                throw;
+            }
+        }
+    }
+}
